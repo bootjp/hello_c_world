4a61f0890419ba6c4fd0620db03227a9438b7a8d
bootjp <oh@bootjp.me>
2018-03-25 11:13:09 +0900

    study pointer

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # hello_c_world
 Project for learning C.
+
+ここではC99をベースに学ぶ
--- main.c
+++ main.c
@@ -1,7 +1,37 @@
 #include <stdio.h>
 
-int main() { // main 関数はintを返さなければならないのでvoid ではなく intが正しいi
-    printf("hello, world\n");
+void hello_world() {
+    printf("Hello c language world!\n");
+}
 
+void swap(int *a, int *b) {
+    int tmp;
+    tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+
+
+int main() {
+    int a = 10;
+    int b = 999999;
+    printf("a = %3d : b = %3d\n",a,b);
+    swap(&a, &b);
+    printf("a = %3d : b = %3d\n",a,b);
+    return 0;
+//    char str[999];
+//    scanf("%s", str);
+//    for(int i = 0; i < 10; i++){
+//        printf("%s\n", str);
+//    }
+//    return 0;
+    int x = 10; // int = 10
+    int* p;// int pointer
+    p = &x;// p = x pointer
+    *p = 5;// x pointer = 5
+    printf("xの中身は%dです。\n", x);
     return 0;
 }
+
+
